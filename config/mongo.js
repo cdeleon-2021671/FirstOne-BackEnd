c171c3f93b2e5e5f5eb358c6a497cb8b2989e4ad
cdeleon-2021671 <cdeleon-2021671@kinal.edu.gt>
2023-10-02 22:54:12 -0600

    Correcion de imagenes

--- config/mongo.js
+++ config/mongo.js
@@ -4,14 +4,14 @@
 // Extraer mongoose de mongodb
 const mongoose = require("mongoose");
 // Utilizar las variables de entorno de produccion
-// require("dotenv").config({ path: ".env.prod" });
+require("dotenv").config({ path: ".env.prod" });
 
 // Iniciar la conexion
 exports.connect = async () => {
   try {
     // Utilizar la ruta para conectarse a la db
-    const uriMongo = `${process.env.URI_MONGO_LOCAL}`;
-    // const uriMongo = `${process.env.URI_MONGO}`;
+    // const uriMongo = `${process.env.URI_MONGO_LOCAL}`;
+    const uriMongo = `${process.env.URI_MONGO}`;
     // Quitar el modo estricto ante cualquier consulta
     mongoose.set("strictQuery", false);
     // Conectar a la base de datos
