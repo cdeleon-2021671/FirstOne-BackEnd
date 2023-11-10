df7bac695e1ae5575e00fe654f5b7f4a8ecbcf84
cdeleon-2021671 <cdeleon-2021671@kinal.edu.gt>
2023-11-10 11:43:14 -0600

    Correcion de rutas

--- src/store/store.routes.js
+++ src/store/store.routes.js
@@ -15,9 +15,6 @@ api.delete("/delete-store/:storeId", ensureAuth, storeController.deleteStore);
 api.get("/get-stores", storeController.getStores);
 api.get("/get-store-by-id/:storeId", storeController.getStoreById);
 api.get("/get-all-stores", storeController.getAllStores);
-api.put("/update-tags", storeController.updateTags);
-api.put("/update-shipping", storeController.updateShippingTerms);
-api.put("/update-payments", storeController.updatePaymentsOptions);
 api.put("/inactive-store/:storeId", ensureAuth, storeController.inactiveStore);
 // Exportar las rutas para ser utilizadas en el app
 module.exports = api;
