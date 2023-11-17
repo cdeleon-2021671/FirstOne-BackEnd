c3bfa6f9084844b8cb3dac7a220bf2bc8f239444
cdeleon-2021671 <cdeleon-2021671@kinal.edu.gt>
2023-11-17 02:49:11 -0600

    Optimizacion de cache

--- src/cache/cache.controller.js
+++ src/cache/cache.controller.js
@@ -74,6 +74,30 @@ const getAutoComplete = async () => {
   }
 };
 
+const getEvents = async () => {
+  try {
+    const { data } = await axios.get(
+      `https://analytics.tienda.gt/newEvent/get-latest-events`
+    );
+    const { result } = data;
+    return await getTrending(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getTrending = async (results) => {
+  try {
+    const { data } = await axios.post(
+      `http://localhost:3200/product/get-trending`,
+      results
+    );
+    const { result } = data;
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
 exports.changeObject = async (req, res) => {
   objects = {};
   const stores = await getStores();
@@ -82,7 +106,16 @@ exports.changeObject = async (req, res) => {
   const products = await getProducts();
   const mostViewed = await getMostViewed();
   const autoComplete = await getAutoComplete();
-  objects = { stores, categories, offers, products, mostViewed, autoComplete };
+  const trending = await getEvents();
+  objects = {
+    stores,
+    categories,
+    offers,
+    products,
+    mostViewed,
+    autoComplete,
+    trending,
+  };
   console.log("Cache actualizado");
   if (res) return res.send({ message: "Cache actualizado" });
 };
